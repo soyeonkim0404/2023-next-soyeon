53f300205e88f3456ff7169558d62801a9ccd369
kimsoyeon <soyeon.kim9104@gmail.com>
2023-12-10 20:36:03 +0900

    two commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'es5',
-  singleQuote: true,
-  printWidth: 90,
-  tabWidth: 2,
-  endOfLine: 'auto',
+  "printWidth": 100, //한줄에 코드 100줄이 넘지 않도록 함.
+  "singleQuote": true, //문자열 사용시 ''사용.
+  "trailingComma": "all", //맨 뒤에 있는 것도 쉼표를 추가함.
+  "tabWidth": 2, //들여쓰기 크기를 2칸으로 함.
+  "bracketSameLine": true // 태그를 같은 줄에 넣음.
 };
