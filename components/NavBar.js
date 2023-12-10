f00d684ab737a560c12b4a92666bb68f3c1efc86
kimsoyeon <soyeon.kim9104@gmail.com>
2023-12-10 16:49:40 +0900

    Initial commit

---
+++ components/NavBar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from './NavBar.module.css'
+export default function NavBar(){
+    const router = useRouter();
+    return(
+        <nav>
+            <Link href="/" className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>
+                Home
+            </Link>
+            <Link href="/about" className={`${styles.link} ${router.pathname === '/about' ? styles.active : ''}`}>
+                About
+            </Link>
+        </nav>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from '@/components/NavBar';
+
+export default function Home(){
+    return (
+        <div>
+            <NavBar/>
+            <h1>home</h1>
+        </div>
+    )
+}
